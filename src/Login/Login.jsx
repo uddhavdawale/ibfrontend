e6aabd24b8be7604d4ead40a1a1cae8a3686c253
uddhavdawale <60895416+uddhavdawale@users.noreply.github.com>
2026-03-25 09:18:03 +0530

    Update Login.jsx
    back end is up

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -24,7 +24,7 @@ function Login() {
     setError('');
     
     try {
-      const response = await fetch('http://localhost:8080/login', {
+      const response = await fetch('https://ibbackend-production.up.railway.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
