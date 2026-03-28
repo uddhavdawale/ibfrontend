1076e00ca132eae911ec78c5293cd1861498344e
uddhavdawale <60895416+uddhavdawale@users.noreply.github.com>
2026-03-28 20:34:10 +0530

    Update vite.config.js
    adding server url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://ibbackend-production.up.railway.app',
         changeOrigin: true,
         secure: false,
       }
